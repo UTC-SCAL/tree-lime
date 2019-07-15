388e79fcfa51644162ea353555777c4fbcbe2646
Alnour Alharin <alnour.ahmed@gmail.com>
2019-07-15 15:11:14 -0400

    Added feature names

--- data.py
+++ data.py
@@ -1,5 +1,8 @@
 from sklearn import tree
 import numpy as np
+from sklearn.externals.six import StringIO
+from sklearn.tree import export_graphviz
+import pydotplus
 
 
 def get_sample(point, means, stds):
@@ -9,21 +12,28 @@ def get_sample(point, means, stds):
 
 
 def get_points_around(point, predictor, samples, samples_size=5, classes_num=2):
-    # todo for now assume that all features are continuous
+    # TODO apply a different criteria for categorical data
     means = np.average(samples, axis=0)
     stds = np.std(samples, axis=0)
     res_samples = []
-    for i in range(samples_size):
+    for _ in range(samples_size):
         current_sample = get_sample(point, means, stds)
         res_samples.append(current_sample)
     return res_samples, predictor(res_samples)
 
 
 def predict_tree(x, y):
-    clf = tree.DecisionTreeClassifier()
+    clf = tree.DecisionTreeClassifier(max_depth=4, min_samples_split=20)
     clf = clf.fit(x, y)
     return clf
 
 
-def visualize_tree(clf):
-    tree.plot_tree(clf)
+def visualize_tree(tree_clf, feature_names):
+    dot_data = StringIO()
+    export_graphviz(tree_clf,
+                    out_file=dot_data,
+                    filled=True, rounded=True,
+                    special_characters=False,
+                    feature_names=feature_names)
+    graph = pydotplus.graph_from_dot_data(dot_data.getvalue())
+    return graph
